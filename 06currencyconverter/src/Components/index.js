8c67424ddf394bef77d5d0b50cb7eca5f618f570
TenzDelek <tibetdelek@gmail.com>
2024-01-12 23:40:49 +0530

    currency

---
+++ 06currencyconverter/src/Components/index.js
@@ -0,0 +1,10 @@
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+//use full in if there are many components to
+//import in the main file .
+
+// in array maping it is best use to put the key
+//because of the performance issue and also for the
+//key it is prefer to use id over index
