66c5320c59c2a2e11e5e98e2f681334ecdddb33f
TenzDelek <tibetdelek@gmail.com>
2024-01-30 11:27:07 +0530

    slug

--- 11megablog/src/components/index.js
+++ 11megablog/src/components/index.js
@@ -1,8 +1,10 @@
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import Container from './Container/Container'
-import LogoutBtn from './Header/LogoutBtn'
-import Logo from './Logo'
-import Button from './Button'
-import Input from './Input'
-export {Header,Footer,Container,Logo,LogoutBtn,Button,Input}
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+import RTE from './RTE'
+import Select from './Select'
+export {Select,RTE, Header, Footer, Container, Logo, LogoutBtn, Button, Input };
